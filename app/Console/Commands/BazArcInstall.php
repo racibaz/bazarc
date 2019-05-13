7f85e7c5db0c2299a43f73b52bed2907eed57b9f
Recai CANSIZ <muhammed.cansiz@izu.edu.tr>
2019-05-13 14:15:04 +0300

    added bazarc:install command

---
+++ app/Console/Commands/BazArcInstall.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class BazArcInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bazarc:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs your migration and seeder commands.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Artisan::call('migrate',['--force' => true]);
+        Artisan::call('db:seed',['--force' => true]);
+
+        return true;
+    }
+}
