7480098326ae7bd04ee40d3a8ccc5538a2a459b4
Recai CANSIZ <muhammed.cansiz@izu.edu.tr>
2019-05-13 15:02:59 +0300

    themes pluging added.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Caffeinated\Themes\Facades\Theme;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Theme::set('bootstrap');
     }
 }
