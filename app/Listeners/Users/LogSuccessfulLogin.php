f148baca30c2e35b562e4ffec745f0b9ee788815
Recai CANSIZ <muhammed.cansiz@izu.edu.tr>
2019-05-14 14:13:00 +0300

    get IP feature add to LogSuccesfulLogin even listener

--- app/Listeners/Users/LogSuccessfulLogin.php
+++ app/Listeners/Users/LogSuccessfulLogin.php
@@ -3,6 +3,7 @@
 namespace App\Listeners\Users;
 
 use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Carbon;
@@ -28,6 +29,7 @@ class LogSuccessfulLogin
     public function handle(Login $event)
     {
         $event->user->last_login = Carbon::now();
+        $event->user->IP = \request()->getClientIp();
         $event->user->save();
     }
 }
