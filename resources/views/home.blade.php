5292fe76d2f51de2f89fab8ffe0f28946a0a7cf2
recai <r.c67@hotmail.com>
2019-05-15 23:12:10 +0300

    home blade fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,34 +16,35 @@
 
                         You are logged in!
 
-                        @if($service == 'facebook')
-                            <div class="title m-b-md">
-                                Welcome {{ $details->user['name']}} ! <br> Your email is : {{
-				$details->user['email'] }} <br> You are {{ $details->user['gender']
-				}}.
-                            </div>
-                        @endif @if($service == 'google')
-                            <div class="title m-b-md">
-                                Welcome {{ $details->name}} ! <br> Your email is : {{
-				$details->email }} <br> Your are {{ $details->user['gender'] }}.
-                            </div>
+                        @if(isset($service))
+                                @if($service == 'facebook')
+                                    <div class="title m-b-md">
+                                        Welcome {{ $details->user['name']}} ! <br> Your email is : {{
+				                $details->user['email'] }} <br> You are {{ $details->user['gender']}}.
+                                    </div>
+                                @endif @if($service == 'google')
+                                    <div class="title m-b-md">
+                                        Welcome {{ $details->name}} ! <br> Your email is : {{
+				                $details->email }} <br> Your are {{ $details->user['gender'] }}.
+                                    </div>
 
-                        @endif @if($service == 'twitter')
-                            <div class="title m-b-md">
-                                Welcome {{ $details->name}} ! <br> Your username is : {{
-				$details->nickname }}<br> Total Tweets : {{
-				$details->user['statuses_count']}}<br> Followers : {{
-				$details->user['followers_count']}}<br> Following : {{
-				$details->user['friends_count']}}
-                            </div>
-                        @endif @if($service == 'github')
-                            <div class="title m-b-md">
-                                Welcome {{ $details->user['name'] }} ! <br> Your email is : {{
-				$details->user['email'] }} <br> Public Repositories :
-                                {{$details->user['public_repos']}}<br> Followers :
-                                {{$details->user['followers']}}
-                            </div>
+                                @endif @if($service == 'twitter')
+                                    <div class="title m-b-md">
+                                        Welcome {{ $details->name}} ! <br> Your username is : {{
+                                $details->nickname }}<br> Total Tweets : {{
+                                $details->user['statuses_count']}}<br> Followers : {{
+                                $details->user['followers_count']}}<br> Following : {{
+                                $details->user['friends_count']}}
+                                    </div>
+                                @endif @if($service == 'github')
+                                    <div class="title m-b-md">
+                                        Welcome {{ $details->user['name'] }} ! <br> Your email is : {{
+				                $details->user['email'] }} <br> Public Repositories :
+                                        {{$details->user['public_repos']}}<br> Followers :
+                                        {{$details->user['followers']}}
+                                    </div>
 
+                                @endif
                         @endif
                     </div>
 
