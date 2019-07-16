1c601eee647085dc41ce6a38cdf2d6b4c713d447
recai <r.c67@hotmail.com>
2019-07-17 00:24:29 +0300

    user index page update

--- resources/views/backend/user/index.blade.php
+++ resources/views/backend/user/index.blade.php
@@ -17,64 +17,15 @@
                     <div class="card-body">
                         <table class="table table-bordered" id="table">
                             <thead>
-                            <tr>
-                                <th>{{trans('user.form.name')}}</th>
-                                <th>{{trans('user.form.email')}}</th>
-                                <th>{{trans('user.form.cell_phone')}}</th>
-                                <th>{{trans('common.form.updated_at')}}</th>
-                                <th width="100px">{{trans('common.form.action')}}</th>
-                            </tr>
+                                <tr>
+                                    <th>{{trans('user.form.name')}}</th>
+                                    <th>{{trans('user.form.email')}}</th>
+                                    <th>{{trans('user.form.cell_phone')}}</th>
+                                    <th>{{trans('common.form.updated_at')}}</th>
+                                    <th width="100px">{{trans('common.form.action')}}</th>
+                                </tr>
                             </thead>
                         </table>
-
-{{--                        <table id="table" class="table table-bordered table-striped">--}}
-{{--                            <thead>--}}
-{{--                                <tr>--}}
-{{--                                    <th>{{trans('user.form.name')}}</th>--}}
-{{--                                    <th>{{trans('user.form.email')}}</th>--}}
-{{--                                    <th>{{trans('user.form.cell_phone')}}</th>--}}
-{{--                                    <th>{{trans('common.form.updated_at')}}</th>--}}
-{{--                                    <th>{{trans('common.form.action')}}</th>--}}
-{{--                                </tr>--}}
-{{--                            </thead>--}}
-{{--                            <tbody>--}}
-{{--                                @foreach($users as $user)--}}
-{{--                                    <tr>--}}
-{{--                                        <td>{{$user->name}}</td>--}}
-{{--                                        <td>{{$user->email}}</td>--}}
-{{--                                        <td>{{$user->cell_phone}}</td>--}}
-{{--                                        <td>{{$user->updated_at}}</td>--}}
-{{--                                        <td>--}}
-{{--                                            <div class="margin">--}}
-{{--                                                <div class="btn-group">--}}
-{{--                                                    <button type="button" class="btn btn-info">{{trans('common.form.action')}}</button>--}}
-{{--                                                    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">--}}
-{{--                                                        <span class="caret"></span>--}}
-{{--                                                        <span class="sr-only">Toggle Dropdown</span>--}}
-{{--                                                    </button>--}}
-{{--                                                    <div class="dropdown-menu" role="menu">--}}
-{{--                                                        <a class="dropdown-item" href="{{route('user.edit', $user->id)}}">@lang('common.form.edit')</a>--}}
-{{--                                                        <a class="dropdown-item" href="{{route('user.show', $user->id)}}">@lang('common.form.show')</a>--}}
-{{--                                                        {{ html()->form('DELETE')->action(route('user.destroy', $user->id))->open() }}--}}
-{{--                                                                {{html()->button(trans('common.form.delete'),'submit')->class('dropdown-item') }}--}}
-{{--                                                        {{ html()->closeModelForm() }}--}}
-{{--                                                    </div>--}}
-{{--                                                </div>--}}
-{{--                                            </div>--}}
-{{--                                        </td>--}}
-{{--                                    </tr>--}}
-{{--                                @endforeach--}}
-{{--                            </tbody>--}}
-{{--                            <tfoot>--}}
-{{--                            <tr>--}}
-{{--                                <th>{{trans('user.form.name')}}</th>--}}
-{{--                                <th>{{trans('user.form.email')}}</th>--}}
-{{--                                <th>{{trans('user.form.cell_phone')}}</th>--}}
-{{--                                <th>{{trans('common.form.updated_at')}}</th>--}}
-{{--                                <th>{{trans('common.form.action')}}</th>--}}
-{{--                            </tr>--}}
-{{--                            </tfoot>--}}
-{{--                        </table>--}}
                     </div>
                     <!-- /.card-body -->
                 </div>
